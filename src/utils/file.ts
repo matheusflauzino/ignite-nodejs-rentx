fe9ceae0135c77d8db7c19f7a4b30d2e6e08bad4
Matheus Flauzino <matheusantonioflauzino@gmail.com>
2022-09-03 21:15:24 -0300

    refactor: ajuste no removeFile - nao utilizado

--- src/utils/file.ts
+++ src/utils/file.ts
@@ -1,3 +1,11 @@
 import fs from "fs";
 
-export const deleteFile = async (filename: string) => {};
+export const deleteFile = async (filename: string) => {
+  try {
+    await fs.promises.stat(filename);
+  } catch {
+    return;
+  }
+
+  await fs.promises.unlink(filename);
+};
